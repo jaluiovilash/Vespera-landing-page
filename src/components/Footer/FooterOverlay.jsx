fbf837f6cb74c4cce956ab3ca1653aab24017906
jaluiovilash <ovilashjalui@gmail.com>
2024-08-20 01:44:04 +0530

    updated

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -1,8 +1,12 @@
-// import React from "react";
+import React from "react";
+
 import "./FooterOverlay.css";
 
-const Footer = () => {
-  return <div>footer</div>;
-};
+const FooterOverlay = () => (
+  <div className="app__footerOverlay">
+    <div className="app__footerOverlay-black" />
+    <div className="app__footerOverlay-img app__bg" />
+  </div>
+);
 
-export default Footer;
+export default FooterOverlay;
